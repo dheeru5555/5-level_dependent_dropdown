93578252655b11b217e41651f3bb84a724da551d
dheeru5555 <56969040+dheeru5555@users.noreply.github.com>
2021-04-09 20:52:53 +0530

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,1 +1,184 @@
 
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 09, 2021 at 03:20 PM
+-- Server version: 5.7.24
+-- PHP Version: 7.2.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mulitiple_dropdown`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `address`
+--
+
+CREATE TABLE `address` (
+  `id` int(11) NOT NULL,
+  `stadium_add` varchar(55) NOT NULL,
+  `city_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `address`
+--
+
+INSERT INTO `address` (`id`, `stadium_add`, `city_id`) VALUES
+(1, 'Chaurchgate West', 1),
+(2, 'MCAS Park,Pimpari', 2),
+(3, 'GREEN PARK,Lucknow Road', 3),
+(4, 'EKANA ,Near Garden', 4),
+(5, 'HOLKAR,Fish Market', 5),
+(6, 'NEHARU,AdolbeyRoad', 6),
+(7, 'NAHAR SINGH,Delhi Highway', 7),
+(8, 'PCA,Gautam Nagar', 8);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `city`
+--
+
+CREATE TABLE `city` (
+  `id` int(11) NOT NULL,
+  `city_name` varchar(55) NOT NULL,
+  `state_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `city`
+--
+
+INSERT INTO `city` (`id`, `city_name`, `state_id`) VALUES
+(1, 'Mumbai', 1),
+(2, 'Pune', 1),
+(3, 'Lucknow', 2),
+(4, 'Kanpur', 2),
+(5, 'Indore', 3),
+(6, 'Gwalior', 3),
+(7, 'Faridabad', 4),
+(8, 'Mohali', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stadium`
+--
+
+CREATE TABLE `stadium` (
+  `id` int(11) NOT NULL,
+  `stadium_name` varchar(55) NOT NULL,
+  `city_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `stadium`
+--
+
+INSERT INTO `stadium` (`id`, `stadium_name`, `city_id`) VALUES
+(1, 'WANKHEDE', 1),
+(2, 'MCAS', 2),
+(3, 'GREEN PARK', 3),
+(4, 'EKANA', 4),
+(5, 'HOLKAR', 5),
+(6, 'NEHARU', 6),
+(7, 'NAHAR SINGH', 7),
+(8, 'PCA', 8);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `states`
+--
+
+CREATE TABLE `states` (
+  `id` int(11) NOT NULL,
+  `state_name` varchar(55) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `states`
+--
+
+INSERT INTO `states` (`id`, `state_name`) VALUES
+(1, 'Maharashtra'),
+(2, 'Uttar Pradesh'),
+(3, 'Madhya Pradesh'),
+(4, 'Haryana'),
+(5, 'Punjab');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `address`
+--
+ALTER TABLE `address`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `city`
+--
+ALTER TABLE `city`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `stadium`
+--
+ALTER TABLE `stadium`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `states`
+--
+ALTER TABLE `states`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `address`
+--
+ALTER TABLE `address`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `city`
+--
+ALTER TABLE `city`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `stadium`
+--
+ALTER TABLE `stadium`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `states`
+--
+ALTER TABLE `states`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
